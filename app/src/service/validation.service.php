87761efc732f016640d97da3c2f2ccc393566e76
saman-jafari <s.jafari@zertificon.com>
2018-02-12 12:21:53 +0100

    redesign folder structure

---
+++ app/src/service/validation.service.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SJA
+ * Date: 12-Feb-18
+ * Time: 12:11 PM
+ */
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,33 @@
-import gulp from 'gulp'
-import sass from 'gulp-sass'
-import sourcemaps from 'gulp-sourcemaps'
-import autoprefixer  from'gulp-autoprefixer'
-import sassdoc  from 'sassdoc'
-import babel from 'gulp-babel'
-import buffer from 'vinyl-buffer'
-import uglify from 'gulp-uglify'
+
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer  from'gulp-autoprefixer';
+import babel from 'gulp-babel';
+import buffer from 'vinyl-buffer';
+import uglify from 'gulp-uglify';
 import imagemin from 'gulp-imagemin';
 
+// input and out put define here
+const inputCss = './app/assets/**/*.scss';
+const inputJs = './app/assets/**/*.js';
+const inputImages = './app/assets/images/**/*';
+const output = './public/';
 
-//css part
-let inputCss = './assets/**/*.scss';
-let inputJs = './assets/**/*.js';
-let output = './public/';
+// options for plugins goes here
 let sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
-};
-let sassdocOptions = {
-    dest: './public/sassdoc'
 };
 
 let autoprefixerOptions = {
     browsers: ['last 4 versions', '> 5%', 'Firefox ESR']
 };
+
+// task are goes here
+
+
+//css tasks
 gulp.task('sass', function(){
    return gulp
        .src(inputCss)
@@ -31,36 +35,9 @@ gulp.task('sass', function(){
        .pipe(sourcemaps.write('./'))
        .pipe(autoprefixer(autoprefixerOptions))
        .pipe(gulp.dest(output))
-       .pipe(sassdoc(sassdocOptions))
        .resume();
 });
-gulp.task('sassdoc', function () {
-    return gulp
-        .src(inputCss)
-        .pipe(sassdoc(sassdocOptions))
-        .resume();
-});
-gulp.task('watch', function() {
-     gulp.watch(inputCss, ['sass']);
-     gulp.watch(inputJs, ['js'])
-        .on('change', function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-gulp.task('prod', ['sassdoc'], function () {
-    gulp
-        .src(inputCss)
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest(output));
-    gulp.src(inputJs)
-        .pipe(babel())
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify()) // Use any gulp plugins you want now
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(output));
-});
+//js tasks
 gulp.task('js', function () {
     return gulp
         .src(inputJs)
@@ -71,8 +48,9 @@ gulp.task('js', function () {
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(output));
 });
+//media tasks
 gulp.task('image',function () {
-    gulp.src('assets/images/*')
+    gulp.src(inputImages)
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.jpegtran({progressive: true}),
@@ -86,4 +64,27 @@ gulp.task('image',function () {
         }))
         .pipe(gulp.dest('public/images'))
 });
-gulp.task('default', ['sass', 'prod','js' ]);
+
+
+gulp.task('watch', function() {
+     gulp.watch(inputCss, ['sass']);
+     gulp.watch(inputJs, ['js'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+});
+gulp.task('prod',['image'], function () {
+    gulp
+        .src(inputCss)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(output));
+    gulp.src(inputJs)
+        .pipe(babel())
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify()) // Use any gulp plugins you want now
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(output));
+});
+gulp.task('default', ['prod']);
